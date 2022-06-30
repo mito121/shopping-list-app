81d43a8bc50a2be45f2b16ea0b489463332242cf
Rasmus Andersen <rasmus.skovhaven@gmail.com>
2022-06-30 14:11:22 +0200

    init

---
+++ components/InputField.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default InputField = (props) => {
+  return (
+    <View>
+        <Text>Lol</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
